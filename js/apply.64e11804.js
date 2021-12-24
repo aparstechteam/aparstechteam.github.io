e2864f45c73241b5839181e8abc02333196204d6
nasimulhasandeep <nasimulhasan513@gmail.com>
2021-12-25 00:48:51 +0600

    updated

---
+++ js/apply.64e11804.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["apply"],{"935e":function(e,t,n){"use strict";n.r(t);var r=n("7a23"),s={class:"w-full mt-32 flex justify-center items-center"},c=Object(r["g"])("h1",{class:"bg-white p-5 rounded-lg shadow-md shadow-blue-600 hover:bg-gray-50 text-3xl ring-2 ring-blue-600"}," Registration has been closed ",-1),a=[c];function i(e,t,n,c,i,o){return Object(r["t"])(),Object(r["f"])("div",s,a)}var o={setup:function(){return{}}},u=n("6b0d"),d=n.n(u);const l=d()(o,[["render",i]]);t["default"]=l}}]);
+//# sourceMappingURL=apply.64e11804.js.map
